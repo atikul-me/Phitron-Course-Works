03d3d72899572544b0310943ec1fe27a31fcb966
Mohammad Atikul Hoque Showrav <261-35-272@diu.edu.bd>
2026-04-06 09:47:36 -0700

    Create arithmetic.c

---
+++ Introduction_to_Programming/Module 2/arithmetic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    a = 10;
+    b = 5;
+    int sum = a+b;
+
+    printf("summation%d\n", sum);
+  
+    int sub = a - b;
+
+    printf("subtraction = %d\n", sub);
+    
+    return 0;
+}
