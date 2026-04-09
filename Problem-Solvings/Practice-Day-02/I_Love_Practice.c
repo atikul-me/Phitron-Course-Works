2b1fe6b0b35e1906ec97a15d10d800a1aa775357
Mohammad Atikul Hoque Showrav <261-35-272@diu.edu.bd>
2026-04-08 22:30:36 -0700

    Update I_Love_Practice.c

--- Problem-Solvings/Practice-Day-02/I_Love_Practice.c
+++ Problem-Solvings/Practice-Day-02/I_Love_Practice.c
@@ -1,5 +1,4 @@
-## Problem Link : https://judge.phitron.io/topics/cm5z7wa3u0007p301xbzdqkuk/cm82xvpjz003qr001kifnvfwu?language=c_103
-
+// Problem Link : https://judge.phitron.io/topics/cm5z7wa3u0007p301xbzdqkuk/cm82xvpjz003qr001kifnvfwu?language=c_103
 
 #include <stdio.h>
 int main()
