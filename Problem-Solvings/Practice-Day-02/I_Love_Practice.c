79f1e64cae6ddc62c834b8c680c050bc7bdf62b6
Mohammad Atikul Hoque Showrav <261-35-272@diu.edu.bd>
2026-04-08 22:22:13 -0700

    Create I_Love_Practice.c

---
+++ Problem-Solvings/Practice-Day-02/I_Love_Practice.c
@@ -0,0 +1,10 @@
+## Problem Link : https://judge.phitron.io/topics/cm5z7wa3u0007p301xbzdqkuk/cm82xvpjz003qr001kifnvfwu?language=c_103
+
+
+#include <stdio.h>
+int main()
+{
+    printf("I Love Practice");
+
+    return 0;
+}
