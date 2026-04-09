5aaaa4a44232cdac6b2269e7df4d7ae09255d078
Mohammad Atikul Hoque Showrav <261-35-272@diu.edu.bd>
2026-04-08 22:31:16 -0700

    Update Zero_or_Non_Zero.c

--- Problem-Solvings/Practice-Day-01/Zero_or_Non_Zero.c
+++ Problem-Solvings/Practice-Day-01/Zero_or_Non_Zero.c
@@ -1,4 +1,4 @@
-## Problem Link : https://judge.phitron.io/topics/cm5z7wa3u0007p301xbzdqkuk/cm82wqkjv002yr001ug30yttk?language=c_103
+// Problem Link : https://judge.phitron.io/topics/cm5z7wa3u0007p301xbzdqkuk/cm82wqkjv002yr001ug30yttk?language=c_103
 
 
 #include <stdio.h>
