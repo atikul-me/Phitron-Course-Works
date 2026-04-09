f06585f7b38e4ff12cc9781053d41ed3ba7adbad
Mohammad Atikul Hoque Showrav <261-35-272@diu.edu.bd>
2026-04-08 22:25:30 -0700

    Create N_times.c

---
+++ Problem-Solvings/Practice-Day-02/N_times.c
@@ -0,0 +1,14 @@
+## Problem Link : https://judge.phitron.io/topics/cm5z7wa3u0007p301xbzdqkuk/cm832etvp004xr0010prtvx7m?language=c_103
+
+#include <stdio.h>
+int main()
+{
+    int i,N;
+    scanf("%d", &N);
+
+    for (i=1; i<=N; i++)
+    {
+        printf("I Love Practice\n");
+    }
+    return 0;
+}
