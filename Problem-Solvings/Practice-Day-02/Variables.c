dde2a5132572744fddb3a8dbde2dd83f84c92ce4
Mohammad Atikul Hoque Showrav <261-35-272@diu.edu.bd>
2026-04-08 22:24:29 -0700

    Create Variables.c

---
+++ Problem-Solvings/Practice-Day-02/Variables.c
@@ -0,0 +1,23 @@
+## Problem Link : https://judge.phitron.io/topics/cm5z7wa3u0007p301xbzdqkuk/cm8328tot004nr001spsd8yba?language=c_103
+
+
+#include <stdio.h>
+int main()
+{
+    int A;
+    long long int B;
+    float C;
+    char D;
+
+    scanf("%d", &A);
+    scanf("%lld", &B);
+    scanf("%f", &C);
+    scanf(" %c", &D);
+
+    printf("%d\n", A);
+    printf("%lld\n", B);
+    printf("%.2f\n", C);
+    printf("%c\n", D);
+
+    return 0;
+}
