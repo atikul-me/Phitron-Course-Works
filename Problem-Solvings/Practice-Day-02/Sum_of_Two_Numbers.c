c68b4d5de6dded2a46c522fc3e5a06d7c9f8993c
Mohammad Atikul Hoque Showrav <261-35-272@diu.edu.bd>
2026-04-08 22:23:33 -0700

    Create Sum_of_Two_Numbers.c

---
+++ Problem-Solvings/Practice-Day-02/Sum_of_Two_Numbers.c
@@ -0,0 +1,15 @@
+## Problem Link : https://judge.phitron.io/topics/cm5z7wa3u0007p301xbzdqkuk/cm82xzmmi003ur001fky2ilnq?language=c_103
+
+
+#include <stdio.h>
+int main()
+{
+    int A, B;
+    scanf("%d %d", &A,&B);
+
+    int sum = A+B;
+
+    printf("%d", sum);
+
+    return 0;
+}
