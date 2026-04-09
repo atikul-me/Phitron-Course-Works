7caed3583e9a94f82f4154571297b47040732e8b
Mohammad Atikul Hoque Showrav <261-35-272@diu.edu.bd>
2026-04-08 22:20:40 -0700

    Create Divisible_by_5_or_Not.c

---
+++ Problem-Solvings/Practice-Day-02/Divisible_by_5_or_Not.c
@@ -0,0 +1,22 @@
+##Problem link : https://judge.phitron.io/topics/cm5z7wa3u0007p301xbzdqkuk/cm832w6940057r001irt8y8ti
+
+
+#include <stdio.h>
+int main()
+{
+    int i, N;
+    scanf("%d", &N);
+
+    for (i=1; i<=N; i++)
+    {
+        if(i%5==0)
+        {
+            printf("%d Yes\n", i);
+        }
+        else
+        {
+            printf("%d No\n", i);
+        }
+    }
+    return 0;
+}
