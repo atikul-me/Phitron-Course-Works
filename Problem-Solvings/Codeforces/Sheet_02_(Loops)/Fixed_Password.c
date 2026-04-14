9c013fcda6d45af77e48536524d0a8c8b9247d8e
Mohammad Atikul Hoque Showrav <265068315+atikul-me@users.noreply.github.com>
2026-04-14 20:56:54 +0600

    Create Fixed_Password.c

---
+++ Problem-Solvings/Codeforces/Sheet_02_(Loops)/Fixed_Password.c
@@ -0,0 +1,20 @@
+// Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/D
+
+#include <stdio.h>
+int main()
+{
+	int X;
+	while(scanf("%d", &X))
+	{
+		if(X==1999)
+		{
+			printf("Correct\n");
+			break;
+		}
+		else
+		{
+			printf("Wrong\n");
+		}
+	}
+	return 0;
+}
