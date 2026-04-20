eb72c306dd25f2b752c00ee2a26d8040554e1132
Mohammad Atikul Hoque Showrav <265068315+atikul-me@users.noreply.github.com>
2026-04-20 20:09:01 +0600

    Create Array_initialization.c

---
+++ Introduction_to_Programming/Module 7/Array_initialization.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int ar[3]={10,20,30};
+    for (int i=0;i<3;i++)
+    {
+        printf("%d ", ar[i]);
+    }
+    printf("\n");
+    int a[4]={5};
+    for (int j=0;j<4;j++)
+    {
+        printf("%d ", a[j]);
+    }
+    return 0;
+}
