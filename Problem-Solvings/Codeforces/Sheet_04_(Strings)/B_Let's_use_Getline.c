2c238fe957bcafdbef5efb6bd7ffe756f0adaa7b
Mohammad Atikul Hoque Showrav <265068315+atikul-me@users.noreply.github.com>
2026-04-22 20:36:15 +0600

    Update B_Let's_use_Getline.c

--- Problem-Solvings/Codeforces/Sheet_04_(Strings)/B_Let's_use_Getline.c
+++ Problem-Solvings/Codeforces/Sheet_04_(Strings)/B_Let's_use_Getline.c
@@ -1,10 +1,12 @@
+// Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/B
+
 #include <stdio.h>
 #include <string.h>
 int main()
 {
-    char s[1000001];
+    char s[1000001]; // Taking extra one size for storing the Null
     fgets(s,1000001,stdin);
-    for (int i=0; s[i]!='\\'; i++)
+    for (int i=0; s[i]!='\\'; i++) // Double backslash for identifying need slash
     {
         printf("%c", s[i]);
     }
