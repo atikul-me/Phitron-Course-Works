eb17273520a79dc32d870698ed9236c5572e6722
Mohammad Atikul Hoque Showrav <265068315+atikul-me@users.noreply.github.com>
2026-05-05 08:49:18 +0600

    Update Pyramid_Pattern.c

--- Introduction_to_Programming/Module 13 (Nested Loop_Pattern)/Pyramid_Pattern.c
+++ Introduction_to_Programming/Module 13 (Nested Loop_Pattern)/Pyramid_Pattern.c
@@ -3,21 +3,21 @@ int main()
 {
 	int n, star = 1, i, j;
 	scanf("%d", &n);
-	int space = n-1;
+	int space = n-1; // Space is n-1 from beginning
 	
-	for(i=1; i<=n; i++)
+	for(i=1; i<=n; i++)  // Line Printing
 	{
-		for(j=1;j<=space;j++)
+		for(j=1;j<=space;j++) // Space Printing
 		{
 			printf(" ");
 		}
-		for(j=1;j<=star;j++)
+		for(j=1;j<=star;j++) // Star Printing
 		{
 			printf("*");
 		}
 		printf("\n");
-		star+=2;
-		space--;
+		star+=2;  // Star increasing in star+2 rate
+		space--; // Space Decreasing each line
 	}
 	return 0;
 }
