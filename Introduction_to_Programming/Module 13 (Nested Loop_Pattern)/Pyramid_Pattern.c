96e012b63aeccff76558878ee346de50d2ac6102
Mohammad Atikul Hoque Showrav <265068315+atikul-me@users.noreply.github.com>
2026-05-05 08:48:39 +0600

    Add files via upload

---
+++ Introduction_to_Programming/Module 13 (Nested Loop_Pattern)/Pyramid_Pattern.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+	int n, star = 1, i, j;
+	scanf("%d", &n);
+	int space = n-1;
+	
+	for(i=1; i<=n; i++)
+	{
+		for(j=1;j<=space;j++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=star;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+		star+=2;
+		space--;
+	}
+	return 0;
+}
