abd6fcbe578b83ad6ab5f9fd70973baffd9c5083
Mohammad Atikul Hoque Showrav <265068315+atikul-me@users.noreply.github.com>
2026-05-08 01:40:02 +0600

    Add files via upload

---
+++ Introduction_to_Programming/Module 14 (Functions)/No_return+no_parameter.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void sum() 
+{
+	int num1,num2;
+	scanf("%d %d",&num1,&num2);
+	int ans = num1 + num2; 
+	printf("%d",ans);        
+}
+
+int main()
+{
+	sum();
+	
+	return 0;
+}
