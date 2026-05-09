610e1deced79c2fdc7e93b42ed7ab711c25f5c49
Mohammad Atikul Hoque Showrav <265068315+atikul-me@users.noreply.github.com>
2026-05-09 10:38:32 +0600

    Add files via upload

---
+++ Introduction_to_Programming/Module 14 (Functions)/Sqrt.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int value = sqrt(25); // int value
+    printf("%d\n", value);
+
+    double value2 = sqrt(15);
+    printf("%lf",value2);
+    return 0;
+}
