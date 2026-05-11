3f385af8f692c465be39c2b036f227cc0ed816f6
Mohammad Atikul Hoque Showrav <265068315+atikul-me@users.noreply.github.com>
2026-05-11 14:07:58 +0600

    Add files via upload

---
+++ Problem-Solvings/Beecrowd_Solutions/1012 - Area.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+int main() 
+{
+    
+ double A, B, C, triangulo, circulo, trapezio, quadrado, retangulo;
+ #define pi 3.14159
+ 
+ scanf("%lf %lf %lf", &A, &B, &C);
+ 
+ triangulo = (A*C)/2;
+ circulo = pi * (C*C);
+ trapezio = ((A+B)/2) * C;
+ quadrado = (B*B);
+ retangulo = (A*B);
+ 
+ printf("TRIANGULO: %.3lf\n", triangulo);
+ printf("CIRCULO: %.3lf\n", circulo);
+ printf("TRAPEZIO: %.3lf\n", trapezio);
+ printf("QUADRADO: %.3lf\n", quadrado);
+ printf("RETANGULO: %.3lf\n", retangulo);
+    
+    return 0;
+}
