e27ef8e004520f414d6dc7e42a689502ea13af7b
Mohammad Atikul Hoque Showrav <265068315+atikul-me@users.noreply.github.com>
2026-05-11 14:08:47 +0600

    Add files via upload

---
+++ Problem-Solvings/Beecrowd_Solutions/1016 - Distance.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() 
+{
+ 
+    int distance, difference_time;
+    
+    scanf("%d", &distance);
+    
+    difference_time = distance * 2;
+    
+    printf("%d minutos\n", difference_time);
+ 
+    return 0;
+}
