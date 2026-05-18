fd7480de6811923a14c67452f2f3c128c0833066
Mohammad Atikul Hoque Showrav <265068315+atikul-me@users.noreply.github.com>
2026-05-18 20:53:58 +0600

    Add files via upload

---
+++ Introduction_to_Programming/Module 18 (2D Array & Matrix)/checking_primary_diagonal_matrix.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+    int r, c;
+    scanf("%d %d", &r, &c);
+    int a[r][c];
+
+    for (int i=0; i<r; i++)
+    {
+        for (int j=0; j<c; j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    bool is_diagonal = true;
+    
+    if (r==c)
+    {
+        // Square matrix 
+        for (int i=0; i<r; i++)
+    {
+        for (int j=0; j<c; j++)
+        {
+            if (i==j)
+            {
+                // We are now at diagonal
+            }
+            else
+            {
+                if (a[i][j]!=0)
+                {
+                    is_diagonal = false;
+                    printf("This is not a primary diagonal matrix\n");
+                }
+            }
+        }
+    }
+}
+
+    if (is_diagonal == true)
+    {
+        printf("This is primary diagonal matrix\n");
+    }
+    return 0;
+}
