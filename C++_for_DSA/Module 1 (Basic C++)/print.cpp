758c6c06cb2850011e515ac6554bc31dfcb2a2b3
Mohammad Atikul Hoque Showrav <265068315+atikul-me@users.noreply.github.com>
2026-05-22 08:14:33 +0600

    Add files via upload

---
+++ C++_for_DSA/Module 1 (Basic C++)/print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 10;
+    double y = 93.984;
+    char z = 'A';
+
+    cout << x << endl << y << endl << z << endl;
+    cout << "This is number "<< x << ". Do you know that?" << endl;
+    return 0;
+}
