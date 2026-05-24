0f8ae5c7aadc54a0b334e997022239950fca10c7
Mohammad Atikul Hoque Showrav <265068315+atikul-me@users.noreply.github.com>
2026-05-24 19:00:34 +0600

    Add files via upload

---
+++ C++_for_DSA/Module 1 (Basic C++)/setprecision.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip> // input-output manipulation. Setprecision needed
+using namespace std;
+int main()
+{
+    double d = 3.412365;
+    cout << fixed << setprecision (6) << d << endl;
+
+    // taking from iomanip and this is the structure
+    
+    return 0;
+}
