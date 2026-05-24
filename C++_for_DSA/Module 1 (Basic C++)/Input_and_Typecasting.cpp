14e50ab81d6261e7eddd0de0e0a4013854c6eb20
Mohammad Atikul Hoque Showrav <265068315+atikul-me@users.noreply.github.com>
2026-05-24 19:00:17 +0600

    Add files via upload

---
+++ C++_for_DSA/Module 1 (Basic C++)/Input_and_Typecasting.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+
+    cin>>a>>b>>c; // using cin for taking input
+    cout<<a<<endl<<b<<endl<<c<<endl;
+
+    char d;
+    cin>>d;
+    cout << d;
+    
+    float e;
+    cin>>e;
+    cout <<(int)e<<endl;  // Typecasting
+    return 0;
+}
