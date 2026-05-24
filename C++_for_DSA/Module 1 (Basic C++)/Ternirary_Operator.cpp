410d2fc408069ccedb23a435139e27ab667b4f2f
Mohammad Atikul Hoque Showrav <265068315+atikul-me@users.noreply.github.com>
2026-05-24 19:00:50 +0600

    Add files via upload

---
+++ C++_for_DSA/Module 1 (Basic C++)/Ternirary_Operator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+
+    x%2==0 ? cout << "Even" : cout << "Odd"; // if-else simplify, only effected if no else if and single if_else statement
+    
+    return 0;
+}
