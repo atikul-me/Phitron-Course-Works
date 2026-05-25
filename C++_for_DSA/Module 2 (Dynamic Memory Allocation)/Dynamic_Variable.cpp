030e7661a1cebc5dde01d10807085150b5efcb9c
Mohammad Atikul Hoque Showrav <265068315+atikul-me@users.noreply.github.com>
2026-05-25 20:21:49 +0600

    Add files via upload

---
+++ C++_for_DSA/Module 2 (Dynamic Memory Allocation)/Dynamic_Variable.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int x = 20;
+	
+	int *p = new int; // new can take us to dynamic memory
+	*p = 100;
+	               // Declare of new for *p and value change
+	cout<<*p<<endl;
+	
+	return 0;
+}
+
+
